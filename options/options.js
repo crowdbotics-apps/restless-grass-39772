1a5366156dec23e82cdf9ede49a439fdb434c303
Crowdbotics <team@crowdbotics.com>
2023-04-07 03:30:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-grass-39772",
+  url: "https://restless-grass-39772.botics.co",
+  api: "https://restless-grass-39772.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
